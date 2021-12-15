a48b06277713886ff1f5dcd09d442276810323ab
Rafayel Khachatryan <rafayel.khachatryan1991@gmail.com>
2021-12-15 21:43:54 +0400

    Create teachers,Students sections.

---
+++ resources/js/components/Admin/Teachers/TeachersIndex.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-100 d-inline-block content-dashboard">
+                <h1>teachers</h1>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "TeachersIndex",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
